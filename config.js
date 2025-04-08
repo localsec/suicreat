438bc670a6b222b860385009d051c9be1f6eded8
Local_sec <116451272+localsec@users.noreply.github.com>
2025-04-08 22:41:29 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 module.exports = {
-  TOTAL_WALLETS: 1000,            // Số lượng ví muốn tạo
-  OUTPUT_DIR: './wallets',        // Thư mục lưu file ví
-  OUTPUT_FILE: 'wallets.txt',     // Tên file output
+  OUTPUT_DIR: './wallets',
+  OUTPUT_FILE: 'wallets.txt',
 };
